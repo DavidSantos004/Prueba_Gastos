ce7a26d201006b613291df5468fa793d39e0220f
DavidSantos004 <ds0299646@gmail.com>
2023-09-21 22:39:50 -0500

    feat: :sparkles: Se agrego apartado para buscar ingresos y egresos registrados y apartado para mirar el total del capital
    Buscar y Total de capital

--- main.js
+++ main.js
@@ -1,55 +1,118 @@
-let myfrom = document.querySelector("form");
-let myTabla =document.querySelector("#myData");
+const myfrom = document.querySelector("form");
+const myTabla = document.querySelector("#myData");
+const input = document.querySelector(".form-control");
+const button = document.querySelector("#boton_bsuqueda");
+const api = "https://650c323b47af3fd22f673fc5.mockapi.io/tabla";
+const datos = document.querySelector(".datos");
+const ingreso = document.querySelector(".ingreso");
+const egreso = document.querySelector(".egreso");
+const total = document.querySelector(".total");
 
-const API_URL= "https://650a69cadfd73d1fab0857b8.mockapi.io/id/valor/user"
 
-addEventListener("DOMContentLoaded" , async ()=>{
-    let res = await (await fetch(API_URL)).json();
-    for(let i=0; i < res.length; i++) {
-        myTabla.insertAdjacentHTML("beforeend" , `
+
+addEventListener("DOMContentLoaded", async () => {
+  let res = await (await fetch(api)).json();
+  let totalIngresos = 0;
+  let totalEgresos = 0;
+  
+  for (let i = 0; i < res.length; i++) {
+
+    myTabla.insertAdjacentHTML(
+      "beforeend",
+      `
             <tr>
                 <td>${res[i].id}</td>
                 <td>${res[i].valor}</td>
                 <td>${res[i].caja}</td>
                 <td><button class="eliminar" data-id="${res[i].id}">Eliminar</button></td>
-            </tr>
-        `);
-        }
-})
+            </tr>`
+    );
 
-myfrom.addEventListener("submit", async(e)=>{ 
-    e.preventDefault();
-    const data =Object.fromEntries(new FormData(e.target));
-    const { valor } = data;
-    data.valor = (typeof valor ==="string") ? Number(valor) : null;
-    let config ={
-        method:"POST",
-        headers:{"content-type": "application/json"},
-        body: JSON.stringify(data)
-    };
-    let res = await fetch (API_URL, config)
-    console.log(res)
-})
+    if (res[i].caja === "ingreso") {
+      totalIngresos = totalIngresos + res[i].valor;
+    } else {
+      totalEgresos = totalEgresos + res[i].valor;
+    }
 
+  }
+
+  ingreso.textContent=`Ingresos: ${totalIngresos}`
+  egreso.textContent=`Egresos: ${totalEgresos}`
+  total.textContent=`Total: ${totalIngresos-totalEgresos}`
+
+});
+
+myfrom.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(e.target));
+  const { valor } = data;
+  data.valor = typeof valor === "string" ? Number(valor) : null;
+  let config = {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(data),
+  };
+  let res = await fetch(api, config);
+  console.log(res);
+});
 
 myTabla.addEventListener("click", async (e) => {
-    if (e.target.classList.contains("eliminar")) {
-        const id = e.target.getAttribute("data-id");
+  if (e.target.classList.contains("eliminar")) {
+    const id = e.target.getAttribute("data-id");
 
-        try {
-            const response = await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+    try {
+      const response = await fetch(`${api}/${id}`, { method: "DELETE" });
 
-            if (response.ok) {
-                console.log("Registro eliminado");
-              
-                const filaAEliminar = e.target.parentElement.parentElement;
-                filaAEliminar.remove();
-            } else {
-                console.error("Error al eliminar el registro");
-            }
-        } catch (error) {
-            console.error("Error al eliminar el registro:", error);
-        }
+      if (response.ok) {
+        console.log("Registro eliminado");
+
+        const filaAEliminar = e.target.parentElement.parentElement;
+        filaAEliminar.remove();
+      } else {
+        console.error("Error al eliminar el registro");
+      }
+    } catch (error) {
+      console.error("Error al eliminar el registro:", error);
     }
+  }
 });
+
+button.addEventListener("click", async (e) => {
+  e.preventDefault();
+  let valor = input.value;
+  let res = await (await fetch(api + "/" + valor)).json();
+  if (valor === res.id) {
+    myTabla.innerHTML = `
+            <tr>
+                <td>${res.id}</td>
+                <td>${res.valor}</td>
+                <td>${res.caja}</td>
+                <td><button class="eliminar" data-id="${res.id}">Eliminar</button></td>
+            </tr>
+        `;
+  } else if (valor === "") {
+    myTabla.innerHTML = ""
+    for (let i = 0; i < res.length; i++) {
+      myTabla.insertAdjacentHTML(
+        "beforeend",
+        `
+                    <tr>
+                        <td>${res[i].id}</td>
+                        <td>${res[i].valor}</td>
+                        <td>${res[i].caja}</td>
+                        <td><button class="eliminar" data-id="${res[i].id}">Eliminar</button></td>
+                        <td><button class="editar" data-id="${res[i].id}">Editar</button></td>
+
+
+                    </tr>
+                `
+      );
+    }
+  } else {
+    myTabla.innerHTML = "NO SE ENCONTRARON DATOS";
+  }
+});
+
+
+
 
